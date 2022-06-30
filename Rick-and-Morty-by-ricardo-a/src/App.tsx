a9986621261a72cbeeb4c300666501d589ddc02e
Ricardo Anjos <ricardoanj.14@gmail.com>
2022-06-30 17:40:51 -0300

    feat: crate header for application

--- Rick-and-Morty-by-ricardo-a/src/App.tsx
+++ Rick-and-Morty-by-ricardo-a/src/App.tsx
@@ -1,6 +1,9 @@
+import Header from "./components/Header";
+
 function App(): JSX.Element {
    return (
-      <div>
+      <div className="flex flex-col items-center">
+        <Header />
         <h1>Rick and Morty Characters</h1>
       </div>
    )
---
+++ Rick-and-Morty-by-ricardo-a/src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <>
+      <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-500">
+        <img
+          className="max-w-xs m-0 pt-0"
+          src='https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABVK-867iNzC3GeSiDQJ7jasFpdN4ySy2Of17S2KxaxbOOtsqax_k_ldd_f5TiDeulU3_lyJmIjtBgPVKLnE1cUK-kRk9yZsO4MXA.png?r=47e'
+          alt="Rick and Morty" />
+      </header>
+    </>
+  )
+}
