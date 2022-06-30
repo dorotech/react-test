28466c5135156b0255946e58ab61ab080da5049d
victor <victor-brambilla@hotmail.com>
2022-06-30 19:34:19 -0400

    feat(data): add pagination

--- rick-and-morty-app/src/data/usecases/remote-get-characters.ts
+++ rick-and-morty-app/src/data/usecases/remote-get-characters.ts
@@ -4,8 +4,8 @@ import { GetCharactersRepository } from '../protocols'
 export class RemoteGetCharacters implements GetCharacters {
   constructor(private readonly getCharactersRepository: GetCharactersRepository) {}
 
-  async perform(): Promise<GetCharacters.Result> {
-    const characters = await this.getCharactersRepository.getCharacters()
+  async perform(params: GetCharacters.Params): Promise<GetCharacters.Result> {
+    const characters = await this.getCharactersRepository.getCharacters(params)
     return characters
   }
 }
