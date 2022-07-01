020ede8ec73c4a0f02de946602454d42b0bb4f72
Ricardo Anjos <ricardoanj.14@gmail.com>
2022-07-01 16:54:07 -0300

    style: add classes tailwind

--- Rick-and-Morty-by-ricardo-a/src/components/Header.tsx
+++ Rick-and-Morty-by-ricardo-a/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-500">
+      <header className="w-full py-5 flex items-center justify-center bg-gray-600 border-b border-gray-500">
         <img
           className="max-w-xs m-0 pt-0"
           src='https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABVK-867iNzC3GeSiDQJ7jasFpdN4ySy2Of17S2KxaxbOOtsqax_k_ldd_f5TiDeulU3_lyJmIjtBgPVKLnE1cUK-kRk9yZsO4MXA.png?r=47e'
