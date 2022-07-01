05cccd536f02915412cdfe4654b7c4b2c9134cbc
victor <victor-brambilla@hotmail.com>
2022-07-01 19:38:06 -0400

    feat(theme): add theme provider

--- rick-and-morty-app/src/App.tsx
+++ rick-and-morty-app/src/App.tsx
@@ -1,9 +1,15 @@
+import { ThemeProvider } from '@mui/material'
 import React from 'react'
 
 import RouterList from './presentation/routes/RouterList'
+import { theme } from './presentation/theme/theme'
 
 function App() {
-  return <RouterList />
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterList />
+    </ThemeProvider>
+  )
 }
 
 export default App
---
+++ rick-and-morty-app/src/presentation/theme/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#03C03C',
+    },
+    secondary: {
+      main: '#653496',
+    },
+  },
+})
