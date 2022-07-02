12ecf2563213fb7d6e43b699d3f1fa39a76b32ea
victor <victor-brambilla@hotmail.com>
2022-07-02 17:06:02 -0400

    feat(home): create home page

--- rick-and-morty-app/src/presentation/pages/home/index.tsx
+++ rick-and-morty-app/src/presentation/pages/home/index.tsx
@@ -1,15 +1,50 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, LinearProgress, Pagination } from '@mui/material'
 import { Header } from '../../components/Header'
-import './style.scss'
 import { FilterComponent } from '../../components/Filter'
+import { CharCard } from '../../components/CharCard'
+import { CharacterModel } from '../../../domain/models'
+import { useCharacter } from '../../hooks/useCharacter'
+import './style.scss'
 
 export const Home = () => {
+  const {
+    data,
+    count,
+    filterType,
+    filterValue,
+    setFilterType,
+    setFilterValue,
+    page,
+    loading,
+    handleChangePage,
+  } = useCharacter()
+
   return (
     <Box className='body'>
       <Header />
       <Box className='main'>
-        <FilterComponent />
+        <FilterComponent
+          filterType={filterType}
+          filterValue={filterValue}
+          setFilterType={setFilterType}
+          setFilterValue={setFilterValue}
+        />
+        {loading && <LinearProgress />}
+        <Box className='characters'>
+          {data?.map((char: CharacterModel) => (
+            <CharCard key={char.id} char={char} />
+          ))}
+        </Box>
+        <Box className='pagination'>
+          <Pagination
+            size='large'
+            count={count}
+            page={page}
+            onChange={handleChangePage}
+            color='primary'
+          />
+        </Box>
       </Box>
     </Box>
   )
