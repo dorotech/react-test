0ed897182729bc772bd633d74f261aee47cfe127
victor <victor-brambilla@hotmail.com>
2022-07-02 18:47:51 -0400

    fix(charCard): fix styles

--- rick-and-morty-app/src/presentation/components/CharCard.tsx
+++ rick-and-morty-app/src/presentation/components/CharCard.tsx
@@ -1,33 +1,49 @@
 import React from 'react'
 import { Button, Card, CardContent, CardMedia, Typography } from '@mui/material'
 import { Lens } from '@mui/icons-material'
-import './CharCard.style.scss'
 import { CharacterModel } from '../../domain/models'
+import { useNavigate } from 'react-router-dom'
+import styles from './CharCard.module.scss'
 
 interface IProps {
   char: CharacterModel
 }
 
 export const CharCard = ({ char }: IProps) => {
+  const navigate = useNavigate()
   return (
-    <Card className='card'>
-      <CardMedia component='img' alt='green iguana' height='200' image={char.image} />
-      <div
-        style={{
-          width: '100%',
-          marginTop: '185px',
+    <Card className={styles.card}>
+      <CardMedia
+        sx={{
+          borderRadius: '50%',
+          width: 'min-content',
+          alignSelf: 'center',
           position: 'absolute',
+          zIndex: '1',
         }}
-      >
-        <CardContent className='containerContent'>
+        component='img'
+        alt='green iguana'
+        height='200'
+        image={char.image}
+      />
+      <div className={styles.cardContent}>
+        <CardContent className={styles.containerContent}>
           <Typography gutterBottom variant='h5' component='div' color='black'>
             {char.name}
           </Typography>
-          <Typography variant='body1' color='black' className='charDescription'>
-            <Lens className={char.status === 'Alive' ? 'alive' : 'dead'} />
+          <Typography variant='body1' color='black' className={styles.charDescription}>
+            <Lens className={char.status === 'Alive' ? styles.alive : styles.dead} />
             Status: {char.status === 'Alive' ? 'Vivo' : 'Morto'}
           </Typography>
-          <Button color='secondary' fullWidth variant='contained' size='medium'>
+          <Button
+            color='secondary'
+            fullWidth
+            variant='contained'
+            size='medium'
+            onClick={() => {
+              navigate(`/character/${char.id}`)
+            }}
+          >
             Ver
           </Button>
         </CardContent>
