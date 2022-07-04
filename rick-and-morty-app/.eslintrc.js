dbe020c9c4c130a8776b60c7de659ede1b9f260d
LeandroFilie <leandro.gfilie@gmail.com>
2022-07-04 18:10:39 -0300

    feat(init): setup project

---
+++ rick-and-morty-app/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': 0,
+  },
+};
---
+++ rick-and-morty-app/src/App.tsx
@@ -0,0 +1,5 @@
+import './styles/global.scss';
+
+export default function App() {
+  return <h1>Hello world</h1>;
+}
