5bb66981042eb1393507a04c367a1d568cfc11c3
LeandroFilie <leandro.gfilie@gmail.com>
2022-07-05 00:18:10 -0300

    feat(Character): add page with details character per id

--- rick-and-morty-app/src/pages/Character/index.tsx
+++ rick-and-morty-app/src/pages/Character/index.tsx
@@ -1,3 +1,68 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+interface CharacterData {
+  id: number,
+  name: string,
+  status: string,
+  image: string,
+  species: string,
+  type: string,
+  gender: 'Male' | 'Female',
+  origin: {
+    name: string,
+  },
+  location: {
+    name: string,
+  },
+
+}
+
 export default function Character() {
-  return <h1>Character</h1>;
+  const { id } = useParams();
+  const [character, setCharacter] = useState<CharacterData>({} as CharacterData);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+      const responseJson = await response.json();
+      setCharacter(responseJson);
+    })();
+  }, []);
+
+  return (
+    <main>
+      <div>
+        <img src={character.image} alt={character.name} />
+        <p>
+          Name:
+          {character.name}
+        </p>
+        <p>
+          Status:
+          {character.status}
+        </p>
+        <p>
+          Species:
+          {character.species}
+        </p>
+        <p>
+          Type:
+          {character.type}
+        </p>
+        <p>
+          Gender:
+          {character.gender}
+        </p>
+        <p>
+          Origin:
+          {character.origin.name}
+        </p>
+        <p>
+          Location:
+          {character.location.name}
+        </p>
+      </div>
+    </main>
+  );
 }
