78e44bfd8950993f962bdea076baed6a3c402eee
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2022-07-05 15:11:18 -0300

    (style)(lint): altera regras de lint e anchor tag
    Altera regras de formatação no lint e remove o text
    decoration e color padrão de uma anchor tag.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
