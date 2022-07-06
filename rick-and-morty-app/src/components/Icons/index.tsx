3a824c268810c61fe70480dee0faae35213d90a3
LeandroFilie <leandro.gfilie@gmail.com>
2022-07-06 02:21:10 -0300

    feat(Icons): add search and empty icons

--- rick-and-morty-app/src/components/Icons/index.tsx
+++ rick-and-morty-app/src/components/Icons/index.tsx
@@ -29,3 +29,15 @@ export function Arrow() {
 
   );
 }
+
+export function Search() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg>
+  );
+}
+
+export function Empty() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" /></svg>
+  );
+}
