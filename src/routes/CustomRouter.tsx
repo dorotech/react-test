15d1c09340e24c92feb7704b33a4897eb1c95046
Gabriel de Souza <gabrielgoomes.desouza@gmail.com>
2022-07-06 01:58:05 -0300

    feat(character): cria pagina que mostra personagem por id
    Cria pagina que mostra personagem por id, episodeos

--- src/routes/CustomRouter.tsx
+++ src/routes/CustomRouter.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import CharacterById from "../pages/CharacterById";
+import CharacterFavorite from "../pages/CharacterFavorite";
+import EpisodeById from "../pages/EpisodeById";
 import Home from "../pages/Home";
 import Layout from "../pages/Layout";
 
@@ -9,10 +11,17 @@ function CustomRouter() {
       <Route path="/" caseSensitive element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/character" caseSensitive element={<Home />} />
+        <Route path="/character/:id" caseSensitive element={<CharacterById />}>
+          <Route
+            path="episode/:episodeId"
+            caseSensitive
+            element={<EpisodeById />}
+          />
+        </Route>
         <Route
-          path="/character/:id"
+          path="/favorites"
           caseSensitive
-          element={<CharacterById />}
+          element={<CharacterFavorite />}
         />
         <Route
           path="*"
