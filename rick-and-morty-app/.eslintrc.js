94e4eea4d98892a84f6db9f6aa159988075e4428
LeandroFilie <leandro.gfilie@gmail.com>
2022-07-07 03:10:02 -0300

    feat(config): add rules eslint

--- rick-and-morty-app/.eslintrc.js
+++ rick-and-morty-app/.eslintrc.js
@@ -24,5 +24,13 @@ module.exports = {
     'react/jsx-filename-extension': 'off',
     'react/react-in-jsx-scope': 'off',
     'import/extensions': 0,
+    'react/jsx-one-expression-per-line': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react/jsx-no-bind': 'off',
+    'no-param-reassign': 'off',
+    'react/jsx-no-constructed-context-values': 'off',
   },
 };
