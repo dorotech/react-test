4de3ba67526ca0151e9a5c70d7cdeacefa959a81
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-07-17 10:00:51 -0300

    feat: preaparo a estrutura para a troca de tema mais configurações globais do css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { ThemeProvider } from 'styled-components';
+import { ThemeDark } from './Themes/Dark';
+import { GlobalStyles } from './Themes/globals';
+
 import { Home } from './Pages/Home';
+import { useState } from 'react';
 
 function App() {
-  return <Home />;
+  const [themeMode, setThemeMode] = useState<'DARK' | 'LIGHT'>('DARK');
+
+  return (
+    <ThemeProvider theme={ThemeDark}>
+      <Home />
+      <GlobalStyles themeMode={themeMode} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
