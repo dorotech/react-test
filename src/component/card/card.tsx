faf13bcea824188e07a322d08c5d87db12ec5028
HashBR <tulio.bart@msn.com>
2022-07-17 16:45:59 -0300

    fix(card): ensure no line break on long text

--- src/component/card/card.tsx
+++ src/component/card/card.tsx
@@ -12,13 +12,11 @@ interface CardProps {
   character: Character;
 }
 
-// TODO: Fix text alignment of status, species, and origin
-// TODO: Fix name breaking layout when too long
 const Card = (props: CardProps) => {
   const { name, status, species, gender, image } = props.character;
   const [isExtraInfoOpen, setIsExtraInfoOpen] = useState(false);
   return (
-    <div className="flex bg-yellow-50 bg-opacity-50 dark:bg-opacity-10 shadow-md rounded-3xl mx-3 my-3">
+    <div className="flex w-80 bg-yellow-50 bg-opacity-50 dark:bg-opacity-10 shadow-md rounded-3xl mx-3 my-3">
       <div className="flex flex-col items-between justify-between m-3">
         <img className="character-image rounded-3xl" src={image} alt="Rick" />
         <div className="shadow-on-name">
@@ -31,28 +29,22 @@ const Card = (props: CardProps) => {
         <div className="flex flex-row items-start character-info-container">
           <div className="character-main-info shadow-md bg-opacity-10 rounded-md bg-black flex flex-col items-start w-9/12 px-2 py-1">
             <div className="character-status flex justify-between w-full">
-              <span className="text-sm text-gray-700 dark:text-sky-50">
+              <span className="text-sm text-gray-700 dark:text-sky-50 w-3/12 text-left">
                 Status
               </span>{" "}
-              <strong className="text-gray-700 dark:text-sky-50 leading-5">
-                {status}
-              </strong>
+              <strong className="info-bold w-9/12">{status}</strong>
             </div>
             <div className="character-species flex justify-between w-full">
-              <span className="text-sm text-gray-700 dark:text-sky-50">
+              <span className="text-sm text-gray-700 dark:text-sky-50 w-3/12 text-left">
                 Species
               </span>{" "}
-              <strong className="text-gray-700 dark:text-sky-50 leading-5">
-                {species}
-              </strong>
+              <strong className="info-bold w-9/12">{species}</strong>
             </div>
             <div className="character-gender flex justify-between w-full">
-              <span className="text-sm text-gray-700 dark:text-sky-50">
+              <span className="text-sm text-gray-700 dark:text-sky-50 w-3/12 text-left">
                 Gender
               </span>{" "}
-              <strong className="text-gray-700 dark:text-sky-50 leading-5">
-                {gender}
-              </strong>
+              <strong className="info-bold w-9/12">{gender}</strong>
             </div>
           </div>
           <button
--- src/component/moreStats/moreStats.tsx
+++ src/component/moreStats/moreStats.tsx
@@ -3,28 +3,30 @@ import { Character } from "../../models/responses/Character";
 interface MoreStatsProps {
   character: Character;
 }
-// TODO: Fiz big names to fit in the card like in card.tsx TODO
+
 const MoreStats = (props: MoreStatsProps) => {
   const { image, type, origin, location } = props.character;
   return (
-    <div className="flex flex-col items-start character-extra-info shadow-md bg-opacity-10 rounded-md bg-black w-full px-2 py-1 mt-3">
+    <div className="flex flex-col items-start character-extra-info shadow-md bg-opacity-10 rounded-md bg-black w-[296px] px-2 py-1 mt-3">
       <div className="flex justify-between w-full">
-        <span className="text-sm text-gray-700 dark:text-sky-50">Type</span>{" "}
-        <strong className="text-gray-700 dark:text-sky-50 leading-5">
-          {type ? type : "unknown"}
-        </strong>
+        <span className="text-sm text-gray-700 dark:text-sky-50 w-3/12 text-left">
+          Type
+        </span>{" "}
+        <strong className="info-bold w-9/12">{type ? type : "unknown"}</strong>
       </div>
       <div className="flex justify-between w-full">
-        <span className="text-sm text-gray-700 dark:text-sky-50">Origin</span>{" "}
-        <strong className="text-gray-700 dark:text-sky-50 leading-5">
+        <span className="text-sm text-gray-700 dark:text-sky-50 w-3/12 text-left">
+          Origin
+        </span>{" "}
+        <strong className="info-bold w-9/12">
           {origin.name ? origin.name : "unknown"}
         </strong>
       </div>
       <div className="flex justify-between w-full">
-        <span className="text-sm text-gray-700 dark:text-sky-50">Location</span>{" "}
-        <strong className="text-gray-700 dark:text-sky-50 leading-5">
-          {location.name}
-        </strong>
+        <span className="text-sm text-gray-700 dark:text-sky-50 w-3/12 text-left">
+          Location
+        </span>{" "}
+        <strong className="info-bold w-9/12">{location.name}</strong>
       </div>
       <button
         className="btn my-1 py-2 w-full h-full"
