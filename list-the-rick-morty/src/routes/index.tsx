26e5fc6db65995c89240c4febe37537d02024631
Leandro Santos <44986666+leandrosax10@users.noreply.github.com>
2023-01-27 16:23:17 -0300

    feat: Instalação da lib Material ui

--- list-the-rick-morty/src/routes/index.tsx
+++ list-the-rick-morty/src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route path="/pagina-inicial" element={<Button>Teste</Button>} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
