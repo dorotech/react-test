14fbbb0fd2e44099d75689f4cbd209663bdb3c9c
Leandro Santos <44986666+leandrosax10@users.noreply.github.com>
2023-01-27 17:35:39 -0300

    feat: Configuração de troca de tema light-dark

--- list-the-rick-morty/src/App.tsx
+++ list-the-rick-morty/src/App.tsx
@@ -1,14 +1,13 @@
-import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { LightTheme } from "./shared/themes";
+import { AppThemeProvider } from "./shared/contexts/ThemeContext";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 };
