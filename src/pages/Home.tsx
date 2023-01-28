ca71d0d301f0048a4ce5f67e167369bdcb7125a9
dev-luizf <luizfurtadomkt@hotmail.com>
2023-01-28 12:12:53 -0300

    feat: implementa a página home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CharList from "../components/CharList";
+import Header from "../components/Header";
+
+export default function Home() {
+ 
+  return (
+    <div>
+      <Header />
+      <CharList />
+    </div>
+  );
+}
