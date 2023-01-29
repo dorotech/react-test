9e8b3133287a7f1f8d6bf97ca9fb759e9790d7fd
dev-luizf <luizfurtadomkt@hotmail.com>
2023-01-29 11:25:40 -0300

    refactor: refatora chamada ao service getAllCharacters

--- src/components/CharList.tsx
+++ src/components/CharList.tsx
@@ -1,21 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import getCharacters from '../services/get/getCharacters';
+import React from 'react';
+import { Character } from '../interfaces/Services';
 import CharCard from './CharCard';
 
-export default function CharList() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const page = searchParams.get('page') || '1';
-  const [characters, setCharacters] = useState<any>([]);
+interface ListProps {
+  characters: Character[];
+}
 
-  const fetchCharacters = async () => {
-    const { data } = await getCharacters(+page);
-    setCharacters(data.results);
-  };
-
-  useEffect(() => {
-    fetchCharacters();
-  }, []);
+export default function CharList({ characters }: ListProps) {
 
   return (
     <div className="charlist-container">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import CharList from '../components/CharList';
 import Header from '../components/Header';
+import Pagination from '../components/Pagination';
+import { Characters } from '../interfaces/Services';
+import getCharacters from '../services/get/getCharacters';
 
 export default function Home() {
+  const [charactersInfo, setCharactersInfo] = useState<Characters | null>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [page, setPage] = useState(searchParams.get('page') || '1');
+
+  const fetchCharacters = async () => {
+    const { data } = await getCharacters(+page);
+    setCharactersInfo(data);
+    setSearchParams({ page });
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  useEffect(() => {
+    fetchCharacters();
+  }, [page]);
+
+  if (!charactersInfo) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <Header />
-      <CharList />
+      <CharList characters={charactersInfo.results}/>
+      <Pagination count={charactersInfo.info.pages} page={page} setPage={(value: string) => setPage(value)}/>
     </div>
   );
 }
