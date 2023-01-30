7028d0029c5494f21ad4d319a37fdeef1983d010
joao-vi <joaovictor.go.ro@gmail.com>
2023-01-30 10:00:29 -0400

    chore: add airbnb eslint rules

--- rick-morty/src/pages/index.tsx
+++ rick-morty/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-import { Inter } from '@next/font/google';
+// import { Inter } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
