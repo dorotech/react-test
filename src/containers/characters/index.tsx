3ae4cf09e3c3efa93b28bf69776dd3e9b0dc33d1
João Victor <jvbrandao98@gmail.com>
2023-01-29 22:04:59 -0300

    feat: create loading icon

--- src/containers/characters/index.tsx
+++ src/containers/characters/index.tsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react";
 import { Box } from "@mui/system";
+import axios from "axios";
 
 const CharactersContainer = () => {
-  return <Box style={{ gridArea: "char" }}>djisaidjsajidsaij</Box>;
+  return (
+    <Box style={{ gridArea: "char" }}>
+      <div>Search Bar</div>
+      <div>Characters</div>
+    </Box>
+  );
 };
 
 export { CharactersContainer };
--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,21 +1,25 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { TopAppBar } from "../../components/topBar";
 import axios from "axios";
 import styles from "./styles.module.scss";
-
 import { CharactersContainer } from "../characters";
 import { ColorModeContext } from "../../contexts";
+import { LoadingIcon } from "../../components/loading";
 
 const HomePage = () => {
-  const { mode } = useContext(ColorModeContext);
+  const [characterList, setCharacterList] = useState([]);
+  const [loading, setLoading] = useState("idle");
 
   const getCharactersList = async () => {
     try {
+      setLoading("loading");
       const response = await axios.get(
         "https://rickandmortyapi.com/api/character?page=2"
       );
-      console.log("response", response);
+      setLoading("loaded");
+      setCharacterList(response.data.results);
     } catch (err) {
+      setLoading("error");
       console.log(err);
     }
   };
@@ -24,21 +28,27 @@ const HomePage = () => {
     getCharactersList();
   }, []);
 
+  const { mode } = useContext(ColorModeContext);
+
   return (
     <div style={{ height: "100%" }} className={styles.container}>
       <TopAppBar />
-      <div
-        className={styles.page_info}
-        style={{
-          gridArea: "characters",
-          margin: "24px",
-          background: mode === "light" ? "#bbbdd6" : "#36385a",
-          color: mode === "light" ? "black" : "white",
-        }}
-      >
-        <div style={{ gridArea: "filters" }}>Filtros</div>
-        <CharactersContainer />
-      </div>
+      {loading === "loading" ? (
+        <LoadingIcon />
+      ) : (
+        <div
+          className={styles.page_info}
+          style={{
+            gridArea: "characters",
+            margin: "24px",
+            background: mode === "light" ? "#bbbdd6" : "#36385a",
+            color: mode === "light" ? "black" : "white",
+          }}
+        >
+          <div style={{ gridArea: "filters" }}>Filtros</div>
+          <CharactersContainer />
+        </div>
+      )}
     </div>
   );
 };
