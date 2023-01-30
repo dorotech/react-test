98b822cb616cd705bb3c888160937e34762ac484
joao-vi <joaovictor.go.ro@gmail.com>
2023-01-30 15:14:29 -0400

    chore: add react query and axios

--- rick-morty/src/pages/_app.tsx
+++ rick-morty/src/pages/_app.tsx
@@ -1,18 +1,22 @@
 import type { AppProps } from 'next/app';
+import { QueryClientProvider } from '@tanstack/react-query/build/lib/QueryClientProvider';
 
 import '../styles/global.css';
 import { Font } from '@/features/common/components/font';
 import { Header } from '@/features/common/components/header';
+import { queryClient } from '@/features/common/lib/react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Font />
-      <main className="w-full h-full max-w-[75rem] mx-auto p-4">
-        <Header />
+      <QueryClientProvider client={queryClient}>
+        <main className="w-full h-full max-w-[75rem] mx-auto p-4">
+          <Header />
 
-        <Component {...pageProps} />
-      </main>
+          <Component {...pageProps} />
+        </main>
+      </QueryClientProvider>
     </>
   );
 }
