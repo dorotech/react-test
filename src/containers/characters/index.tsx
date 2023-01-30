74ba9c39021f5a8e8378b5a43bbc35bc1995f43a
João Victor <jvbrandao98@gmail.com>
2023-01-30 17:17:32 -0300

    feat: started pagination

--- src/containers/characters/index.tsx
+++ src/containers/characters/index.tsx
@@ -1,12 +1,80 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Box } from "@mui/system";
 import axios from "axios";
+import { TablePagination } from "@mui/material";
+import { LoadingIcon } from "../../components/loading";
 
 const CharactersContainer = () => {
+  const [characterList, setCharacterList] = useState([]);
+  const [loading, setLoading] = useState("idle");
+  const [paginationInfo, setPaginationInfo] = useState<any>([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(20);
+
+  const getCharactersList = async (query: string) => {
+    try {
+      setLoading("loading");
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/character?page=${query}`
+      );
+      setLoading("loaded");
+      setCharacterList(response.data.results);
+      setPaginationInfo(response.data.info);
+    } catch (err) {
+      setLoading("error");
+      console.log(err);
+    }
+  };
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value));
+    setPage(0);
+  };
+
+  useEffect(() => {
+    const currentPage = (rowsPerPage * (page + 1)) / 20;
+    if (rowsPerPage * (page + 1) > 20) getCharactersList(`${currentPage}`);
+    else getCharactersList(`${1}`);
+  }, [page, rowsPerPage]);
+
   return (
     <Box style={{ gridArea: "char" }}>
       <div>Search Bar</div>
-      <div>Characters</div>
+      <div>
+        {loading === "loading" ? (
+          <LoadingIcon />
+        ) : (
+          <div>
+            {characterList.map((char: any) => {
+              return (
+                <div>
+                  <span>{char.name}</span>
+                  <span>{char.species}</span>
+                  <span>{char.status}</span>
+                </div>
+              );
+            })}
+            <TablePagination
+              rowsPerPageOptions={[20]}
+              component="div"
+              count={paginationInfo.count}
+              page={page}
+              onPageChange={handleChangePage}
+              rowsPerPage={rowsPerPage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </div>
+        )}
+      </div>
     </Box>
   );
 };
--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -7,48 +7,24 @@ import { ColorModeContext } from "../../contexts";
 import { LoadingIcon } from "../../components/loading";
 
 const HomePage = () => {
-  const [characterList, setCharacterList] = useState([]);
-  const [loading, setLoading] = useState("idle");
-
-  const getCharactersList = async () => {
-    try {
-      setLoading("loading");
-      const response = await axios.get(
-        "https://rickandmortyapi.com/api/character?page=2"
-      );
-      setLoading("loaded");
-      setCharacterList(response.data.results);
-    } catch (err) {
-      setLoading("error");
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    getCharactersList();
-  }, []);
-
   const { mode } = useContext(ColorModeContext);
 
   return (
     <div style={{ height: "100%" }} className={styles.container}>
       <TopAppBar />
-      {loading === "loading" ? (
-        <LoadingIcon />
-      ) : (
-        <div
-          className={styles.page_info}
-          style={{
-            gridArea: "characters",
-            margin: "24px",
-            background: mode === "light" ? "#bbbdd6" : "#36385a",
-            color: mode === "light" ? "black" : "white",
-          }}
-        >
-          <div style={{ gridArea: "filters" }}>Filtros</div>
-          <CharactersContainer />
-        </div>
-      )}
+
+      <div
+        className={styles.page_info}
+        style={{
+          gridArea: "characters",
+          margin: "24px",
+          background: mode === "light" ? "#bbbdd6" : "#36385a",
+          color: mode === "light" ? "black" : "white",
+        }}
+      >
+        <div style={{ gridArea: "filters" }}>Filtros</div>
+        <CharactersContainer />
+      </div>
     </div>
   );
 };
