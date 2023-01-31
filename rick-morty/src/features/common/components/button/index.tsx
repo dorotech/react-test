b0717b1c320ac0c68e62a7a49eee4f7301e2e386
joao-vi <joaovictor.go.ro@gmail.com>
2023-01-31 07:13:57 -0400

    refactor: button styles

--- rick-morty/src/features/common/components/button/index.tsx
+++ rick-morty/src/features/common/components/button/index.tsx
@@ -1,13 +1,15 @@
-import { HTMLAttributes } from 'react';
+import { cva } from 'class-variance-authority';
+import { ButtonHTMLAttributes } from 'react';
 
-type IButton = HTMLAttributes<HTMLButtonElement>;
+const styles = cva(
+  'flex items-center justify-center bg-zinc-800 hover:bg-zinc-700 py-2 px-3 rounded border border-zinc-600 transition focus:ring-2 ring-offset-2 ring-offset-black ring-amber-300'
+);
+
+type IButton = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button({ children, className, ...rest }: IButton) {
   return (
-    <button
-      {...rest}
-      className={`${className} flex items-center justify-center bg-zinc-800 p-2 rounded border border-zinc-600 transition focus:ring ring-offset-2 ring-offset-black ring-amber-300`}
-    >
+    <button {...rest} className={styles({ className })}>
       {children}
     </button>
   );
