fe0346769291355b0908838ba09e442b6e5b887c
joao-vi <joaovictor.go.ro@gmail.com>
2023-01-31 14:35:07 -0400

    fix: getSearchParamsToObject edge case bug

--- rick-morty/src/features/common/utils/get-search-params-to-object/index.ts
+++ rick-morty/src/features/common/utils/get-search-params-to-object/index.ts
@@ -1,6 +1,8 @@
 export const getSearchParamsToObject = (search: string) => {
   const [_, params] = search.split('?');
 
+  if (!params) return {};
+
   const parsedSearch = params.split('&').reduce((object, param) => {
     const [key, value] = param.split('=');
 
