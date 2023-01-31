de01629d0c9a6fe98c824ef506291205b3c6444d
dev-luizf <luizfurtadomkt@hotmail.com>
2023-01-31 03:20:00 -0300

    feat: implementa componente de erro e de página não encontrada

---
+++ src/components/Error.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Morty from "../assets/morty.png";
+import "./NotFound.css";
+
+export default function Error() {
+  return (
+    <div className="not-found-container">
+      <h1>Something went wrong, try again later!</h1>
+      <img src={Morty} alt="morty scared" />
+    </div>
+  );
+}
