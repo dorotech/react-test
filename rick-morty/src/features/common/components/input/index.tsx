0a7822d6e25d9bd1c2301bb8ea06c3e54fd80aa1
joao-vi <joaovictor.go.ro@gmail.com>
2023-01-31 10:55:27 -0400

    refactor: some style tweaks on input component

--- rick-morty/src/features/common/components/input/index.tsx
+++ rick-morty/src/features/common/components/input/index.tsx
@@ -6,8 +6,8 @@ interface IInput extends InputHTMLAttributes<HTMLInputElement> {
 
 export function Input({ className, icon, ...rest }: IInput) {
   return (
-    <div className="flex items-center justify-between transition rounded border border-zinc-700 bg-zinc-800 p-2 pl-3 focus-within:ring-2 ring-amber-300 ring-offset-4 ring-offset-zinc-900">
-      <input {...rest} className={`${className} bg-transparent outline-none`} />
+    <div className="flex items-center justify-between pr-2 transition rounded border border-zinc-700 bg-zinc-800 focus-within:ring-2 ring-amber-300 ring-offset-4 ring-offset-zinc-900">
+      <input {...rest} className={`${className} w-full p-2 pl-3 bg-transparent outline-none`} />
 
       {!!icon && icon}
     </div>
