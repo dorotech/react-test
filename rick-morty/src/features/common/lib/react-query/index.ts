2f6061f5c9a87ebd069a8aba46249e752a9367d4
joao-vi <joaovictor.go.ro@gmail.com>
2023-02-02 08:13:06 -0400

    chore: react query retry and refetch configs

--- rick-morty/src/features/common/lib/react-query/index.ts
+++ rick-morty/src/features/common/lib/react-query/index.ts
@@ -1,3 +1,10 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
